1fe088717203f727db31bcb97b9cee0058e028a7
Nico-bit1 <aravenanicolas40@gmail.com>
2025-04-09 09:28:53 -0400

    Add files via upload

---
+++ actividad.py
@@ -0,0 +1,34 @@
+#variable = 10
+#variablefloat = 3.4
+#variablestr = "hola"
+#variableBool = True
+
+
+
+#Nombre = input("Por favor, dimr tu nombre: ")
+#print("bienvenido,", Nombre)
+
+#suma
+#v01 = int(input("Dime tu numero entero: "))
+#v02 = float(input("Dime tu numero decimal "))
+
+# Operacion
+
+#R01 = v01 + v02 
+
+# pedir al usario que ingrese dos numeros 
+num1 = float(input("Ingresar el primer numero: "))
+num2 = float(input("Ingresar el primer numero: "))
+
+
+# Operaciones 
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
+# Mostrar el resultado 
+print(f"suma: {suma} ")
+print(f"resta: {resta} ")
+print(f"multiplicacion: {multiplicacion} ")
+print(f"division: {division} ")
